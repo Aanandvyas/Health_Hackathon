c9c19c587e1d0391bf6e49bac89689644653fd8b
anand vyas <anandvyas0707@gmail.com>
2025-08-07 17:10:11 +0530

    debugssss

--- vite.config.ts
+++ vite.config.ts
@@ -16,4 +16,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  // This line tells Vite to serve the app from the /Health-Menta/ sub-path
+  base: '/Health-Menta',
 });
