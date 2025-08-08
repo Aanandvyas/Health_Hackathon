5248529bc9c0e6248f1cb60c54dfd9aef2f34b64
anand vyas <anandvyas0707@gmail.com>
2025-08-08 09:20:31 +0530

    debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import PageLoader from './Components/UI/PageLoader';
