24af094ef347bd28c8d1e1b11794d3093cb3115f
anand vyas <anandvyas0707@gmail.com>
2025-08-11 01:21:07 +0530

    name changes

---
+++ src/assets/images/index.ts
@@ -0,0 +1,47 @@
+// This file imports all images and exports them as a single object for easy access.
+
+import neurology from './icons/neurology.png';
+import cardiology from './icons/cardiology.png';
+import orthopedics from './icons/orthopoedics.png';
+import surgery from './icons/surgery.png';
+import dentistry from './icons/dentistry.png';
+import radiology from './icons/radiology.png';
+import urology from './icons/urology.png';
+import medicine from './icons/medicine.png';
+import seeMoreImage from './icons/seemoreimage.png';
+
+import banner1 from './banner1.png';
+import banner2 from './banner2.png';
+
+import doc1 from './doctors/doc1.png';
+import doc2 from './doctors/doc2.png';
+import doc3 from './doctors/doc3.png';
+import doc4 from './doctors/doc4.png';
+
+import user1 from './users/user1.jpg';
+import user2 from './users/user2.jpg';
+import user3 from './users/user3.jpg';
+
+import logo from './logo.png';
+
+export const images = {
+  neurology,
+  cardiology,
+  orthopedics,
+  surgery,
+  dentistry,
+  radiology,
+  urology,
+  medicine,
+  seeMoreImage,
+  banner1,
+  banner2,
+  doc1,
+  doc2,
+  doc3,
+  doc4,
+  user1,
+  user2,
+  user3,
+  logo,
+};
